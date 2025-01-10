e767f1913da0029f901d89e124738db48267d227
Adli Renhoren <62419296+adliren54@users.noreply.github.com>
2025-01-10 09:04:10 +0000

    update

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import os, requests, json
 import google.generativeai as genai
-import os
 
-geminiAPI = os.environ['geminiapi']
+newsKey = os.environ['newsapi']
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 
-genai.configure(api_key=geminiAPI)
-model = genai.GenerativeModel("gemini-pro")
-response = model.generate_content("Explain how AI works")
-print(response.text)
+model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest")
+
+
+country = "us"
+url = f"https://newsapi.org/v2/top-headlines?country={country}&apiKey={newsKey}"
+
+result = requests.get(url)
+data = result.json()
+#print(json.dumps(data, indent=2))
+
+counter = 0
+for article in data['articles']:
+    counter += 1
+    if counter > 5:
+        break
+    prompt = f"Summarize {article['url']} in one sentence."
+    chat = model.start_chat()
+    response = chat.send_message(prompt)
+    print(response.text)
